2a59b2825d6d7844c6c1316c24a1bf2430398783
Boni Garcia <boni.garcia@uc3m.es>
2024-04-29 12:46:13 +0200

    Include hello-world project for Selenium using Maven and JUnit 5

---
+++ selenium/src/test/java/io/github/bonigarcia/selenium/HelloWorldSeleniumTest.java
@@ -0,0 +1,71 @@
+/*
+ * (C) Copyright 2024 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.selenium;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.openqa.selenium.OutputType.FILE;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class HelloWorldSeleniumTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void test() throws Exception {
+        driver.get(
+                "https://bonigarcia.dev/selenium-webdriver-java/login-form.html");
+
+        // Log in
+        driver.findElement(By.id("username")).sendKeys("user");
+        driver.findElement(By.id("password")).sendKeys("user");
+        driver.findElement(By.cssSelector("button")).click();
+        String bodyText = driver.findElement(By.tagName("body")).getText();
+
+        assertThat(bodyText).contains("Login successful");
+
+        // Take screenshot
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(FILE);
+        Path destination = Paths.get("hello-world-selenium.png");
+        Files.move(screenshot.toPath(), destination, REPLACE_EXISTING);
+
+        assertThat(destination).exists();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+}
