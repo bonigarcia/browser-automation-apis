9b89523c0b2700a05e93d04a2cd73e9f67ee482f
Boni Garcia <boni.garcia@uc3m.es>
2024-05-10 12:29:58 +0200

    Enhance cypress config file

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "cypress-hello-world",
   e2e: {
-	supportFile: false,
+	  supportFile: false,
   },
 });
