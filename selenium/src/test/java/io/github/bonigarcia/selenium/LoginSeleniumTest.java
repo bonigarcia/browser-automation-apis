0e124f01e1044f1fb35e8bb534c36f29c05c844a
Boni Garcia <boni.garcia@uc3m.es>
2024-05-20 10:53:13 +0200

    Remove non necessary assets

--- selenium/src/test/java/io/github/bonigarcia/selenium/LoginSeleniumTest.java
+++ selenium/src/test/java/io/github/bonigarcia/selenium/LoginSeleniumTest.java
@@ -24,7 +24,6 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.Duration;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
