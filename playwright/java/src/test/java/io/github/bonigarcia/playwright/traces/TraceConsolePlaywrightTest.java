c05367d09cf2fd45b87aea9ca2caa611402abe64
Boni Garcia <boni.garcia@uc3m.es>
2025-08-17 11:24:00 +0200

    Include trace console test

---
+++ playwright/java/src/test/java/io/github/bonigarcia/playwright/traces/TraceConsolePlaywrightTest.java
@@ -0,0 +1,101 @@
+/*
+ * (C) Copyright 2025 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.playwright.traces;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Tracing;
+
+class TraceConsolePlaywrightTest {
+
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium()
+                .launch(new BrowserType.LaunchOptions().setHeadless(false));
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+
+        // Start tracing
+        context.tracing().start(new Tracing.StartOptions().setScreenshots(true)
+                .setSnapshots(true).setSources(true));
+
+        page = context.newPage();
+    }
+
+    @Test
+    void test() {
+        List<String> logs = new ArrayList<>();
+
+        // Capture console messages
+        page.onConsoleMessage(msg -> {
+            String log = "[console " + msg.type() + "] " + msg.text();
+            System.out.println(log);
+            logs.add(log);
+        });
+
+        // Capture unhandled exceptions
+        page.onPageError(err -> {
+            String log = "[page error] " + err;
+            System.out.println(log);
+            logs.add(log);
+        });
+
+        // Open system under test (SUT)
+        page.navigate(
+                "https://bonigarcia.dev/selenium-webdriver-java/console-logs.html");
+
+        // Assert logs
+        assertThat(logs).hasSizeGreaterThanOrEqualTo(5);
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.tracing().stop(new Tracing.StopOptions()
+                .setPath(Paths.get("console-traces.zip")));
+        context.close();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+}
